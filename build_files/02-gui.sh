524942cf574d3936bf00f2dc022c48c41128f817
linuxdoggo <themooskiifox@proton.me>
2025-10-26 13:44:10 +0100

    Update 02-gui.sh

--- build_files/02-gui.sh
+++ build_files/02-gui.sh
@@ -24,6 +24,12 @@ dnf5 -y copr enable zirconium/packages
 dnf5 -y install matugen 
 dnf5 -y copr disable zirconium/packages
 
+dnf install \
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+
+dnf install \
+  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
 # this installs a package from fedora repos
 dnf5 install -y \
     google-roboto-fonts \
