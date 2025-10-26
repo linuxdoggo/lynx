09e6526f91cd1e27581aa9549d0234f9108f1623
linuxdoggo <themooskiifox@proton.me>
2025-10-26 12:00:37 +0100

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,12 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y \
+  plymouth
+  plymouth-system-theme
+
+dnf -y remove console-login-helper-messages \
+    chrony
 
 # Use a COPR Example:
 #
