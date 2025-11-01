2bafa034d187f3b9aa8d4826cfe3593d58c0cdc4
Riley <themooskiifox@proton.me>
2025-11-01 19:20:52 +0100

    removed fedora flatpak

--- build_files/99-end.sh
+++ build_files/99-end.sh
@@ -9,5 +9,5 @@ mkdir -p /etc/flatpak/remotes.d/
 curl --retry 3 -Lo /etc/flatpak/remotes.d/flathub.flatpakrepo https://dl.flathub.org/repo/flathub.flatpakrepo
 
 #begone fedora flatpak repo
-#rm -rf /usr/lib/systemd/system/flatpak-add-fedora-repos.service
-#systemctl enable flatpak-add-flathub-repos.service
+rm -rf /usr/lib/systemd/system/flatpak-add-fedora-repos.service
+systemctl enable flatpak-add-flathub-repos.service
